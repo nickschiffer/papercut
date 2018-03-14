a51c069ae64312a01abcc1fbeeff002cc07a0f7c
nickschiffer <nick@schiffer.us>
2018-03-14 14:43:40 -0700

    Added new masthead img, updated landing descriptions, sticky navbar, cleaned up and styled alerts, modified testimonials, fixed signup buttons

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( devise.css )
 Rails.application.config.assets.precompile += %w( landing-page.min.css )
+Rails.application.config.assets.precompile += %w( alerts.css )
