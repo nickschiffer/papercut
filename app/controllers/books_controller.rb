9c6c8cdbf9803535a84f7f5f0ad7bf31f9d2c949
nickschiffer <nick@schiffer.us>
2018-03-24 00:16:45 -0700

    fixed image upload and another edit post route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -71,6 +71,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :condition, :ISBN, :slug,:post_id)
+      params.require(:book).permit(:title, :author, :condition, :ISBN, :slug, :post_id, :image)
     end
 end
