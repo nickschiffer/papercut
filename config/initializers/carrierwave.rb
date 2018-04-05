18e907df4f96b75aa6434e0380ca0878197086fd
nickschiffer <nick@schiffer.us>
2018-04-04 20:43:59 -0700

    revert

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -25,12 +25,6 @@
       config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
       config.storage = :fog
     end
-
-     config.fog_directory  = ENV["AWS_BUCKET_NAME"]                  # required
-     #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-     #config.fog_public     = false                                  # optional, defaults to true
-     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-     config.storage = :fog
     
   end
   
