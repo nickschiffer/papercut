01e6ee2273a9d7e1a9b59b37c363accebe98c5b0
nickschiffer <nick@schiffer.us>
2018-04-04 20:17:45 -0700

    attempted fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,9 +13,7 @@ CarrierWave.configure do |config|
   # Use AWS storage if in production
   if Rails.env.production?
     CarrierWave.configure do |config|
-      config.storage = :fog
-    end
-    config.fog_credentials = {
+      config.fog_credentials = {
     :provider               => ENV["PROVIDER"],                   # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],          # required
     :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],      # required
@@ -25,6 +23,9 @@ CarrierWave.configure do |config|
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+      config.storage = :fog
+    end
+    
   end
   
   
