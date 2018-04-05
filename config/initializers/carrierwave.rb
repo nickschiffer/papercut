85edf98cf0b07696b2be0a0f19fd88d9fb6356a5
nickschiffer <nick@schiffer.us>
2018-04-04 21:34:31 -0700

    fixed?

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,18 +13,19 @@
   # Use AWS storage if in production
   if Rails.env.production?
     CarrierWave.configure do |config|
+      config.fog_provider = 'fog/aws'                                 # required
       config.fog_credentials = {
-    :provider               => ENV["PROVIDER"],                   # required
-    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],          # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],      # required
-    :region                 => ENV["AWS_REGION"]                  # optional, defaults to 'us-east-1'
-  }
-      config.fog_directory  = ENV["AWS_BUCKET_NAME"]                  # required
-      #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-      #config.fog_public     = false                                  # optional, defaults to true
-      config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-      config.storage = :fog
+        :provider               => ENV["PROVIDER"],                   # required
+        :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],          # required
+        :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],      # required
+        :region                 => ENV["AWS_REGION"]                  # optional, defaults to 'us-east-1'
+      }
     end
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"]                  # required
+    #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+    config.fog_public     = false                                  # optional, defaults to true
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+    config.storage = :fog
     
   end
   
