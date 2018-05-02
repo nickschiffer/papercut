189fa8667d78b3293eaa17f98ac375737b3d99ba
nickschiffer <nick@schiffer.us>
2018-05-02 14:06:52 -0700

    fixed type cast for sales migration

--- db/migrate/20180419220628_create_sales.rb
+++ db/migrate/20180419220628_create_sales.rb
@@ -6,8 +6,8 @@ class CreateSales < ActiveRecord::Migration[5.1]
       t.integer :book_id
       t.integer :trade_id
       t.integer :state, :default => 0
-      t.boolean :verified_by_buyer, :default => 0
-      t.boolean :verified_by_seller, :default => 0
+      t.boolean :verified_by_buyer, :default => false
+      t.boolean :verified_by_seller, :default => false
       
       t.timestamps
     end
